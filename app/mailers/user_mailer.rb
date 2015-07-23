0628f24ae009bbdf4ef3eeeca10603aff3504124
Ivancha-Igor <ivan4afamily@gmail.com>
2015-07-23 14:36:57 +0300

    Minor commit to user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def notify_admin(params)
     @textarea, @custom_number, @phone_number = params.values_at(:textarea, :custom_number, :phone_number).join(' ')
     mail(to: 'ivan4afamily@gmail.com',
-        subject: 'Welcome to My Awesome Site')
+         subject: 'Welcome to My Awesome Site')
   end
 end
