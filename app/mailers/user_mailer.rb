4e0062dfe6db151bbcb52615af1d888093669939
Ivancha-Igor <ivan4afamily@gmail.com>
2015-07-24 13:24:43 +0300

    Change in mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   def notify_admin(params)
     @textarea, @custom_number, @phone_number = params.values_at(:textarea, :custom_number, :phone_number).join(' ')
-    mail(to: 'ivan4afamily@gmail.com',
+    mail(to: 'ujinphotosite@gmail.com',
          subject: 'Welcome to My Awesome Site')
   end
 end
