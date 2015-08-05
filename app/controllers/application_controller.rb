97a99076e63add7030a21c3459569729ca182253
Ivancha-Igor <ivan4afamily@gmail.com>
2015-08-05 23:04:17 +0300

    Redirect from www to root domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :strip_www
+
+  def strip_www
+    if request.env["HTTP_HOST"] == "www.ujinphoto.ck.ua"
+      redirect_to "http://ujinphoto.ck.ua/"
+    end
+  end
 end
